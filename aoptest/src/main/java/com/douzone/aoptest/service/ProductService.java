9277020c3f3589227dec1f802823533fb0d6b24a
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-10-20 16:32:09 +0900

    aop:porxy#2

--- aoptest/src/main/java/com/douzone/aoptest/service/ProductService.java
+++ aoptest/src/main/java/com/douzone/aoptest/service/ProductService.java
@@ -6,9 +6,13 @@ import com.douzone.aoptest.vo.ProductVo;
 
 @Service
 public class ProductService {
-	
-	public ProductVo find(String name) {
+	public ProductVo find(String name) {		
 		System.out.println("[ProductService] finding...");
+
+		if(1-1 == 0) {
+			throw new RuntimeException("Find Exception");
+		}
+
 		return new ProductVo(name);
 	}
 }
