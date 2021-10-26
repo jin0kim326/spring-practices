2b46d5ba7dc2b380eb1bbdf53df2866c0a785444
jin0kim326 <53967482+jin0kim326@users.noreply.github.com>
2021-10-26 16:24:33 +0900

    XML Config & javaConfig little

---
+++ container/src/main/java/com/douzone/container/user/test/XmlConfigTest.java
@@ -0,0 +1,107 @@
+package com.douzone.container.user.test;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
+
+import com.douzone.container.user.User;
+import com.douzone.container.user.User1;
+
+public class XmlConfigTest {
+	public static void main(String[] args) {
+		// XML Auto Configuration (Annotation Scanning)
+//		testBeanFactory01();
+		
+		//XML Bean Configuration (Explicit Configuration)
+//		testBeanFactory02();
+		
+		//XML Auto Configuration (Annotation Scanning)
+//		testApplicationContext01();
+		testApplicationContext02();
+	}
+	
+	//XML Bean Configuration (Explicit Configuration)
+	private static void testApplicationContext02() {
+		BeanFactory ac = new ClassPathXmlApplicationContext("com/douzone/container/config/user/applicationContext02.xml");
+		
+		// id로 빈 가져오기  
+		User user = (User)ac.getBean("user");
+		System.out.println(user);
+		
+		// name으로 빈 가져오기
+		user = (User)ac.getBean("usr");
+		System.out.println(user);
+		
+		// 타입으로 빈 가져오기
+		// 같은 타입의 빈이 두개 이상 있을때 타입으로 가져오면 에러 발생
+		/* 해결방법
+		 * 1. id + type
+		 * 2. name + type
+		 */
+		user = ac.getBean("user", User.class);
+		System.out.println(user);
+		
+		// 2개 파라미터로 생성된 빈 가져오기1
+		user = ac.getBean("user3", User.class);
+		System.out.println(user);
+		
+		// 2개 파라미터로 생성된 빈 가져오기2
+		user = ac.getBean("user4", User.class);
+		System.out.println(user);
+		
+		// setter를 사용한 빈 가져오기 
+		user = ac.getBean("user5", User.class);
+		System.out.println(user);
+		
+		// setter를 사용한 빈 가져오기2 (DI)
+		user = ac.getBean("user6", User.class);
+		System.out.println(user);
+		
+		// setter를 사용한 빈 가져오기3 (List Property)
+		user = ac.getBean("user7", User.class);
+		System.out.println(user);
+	}
+	
+	//XML Auto Configuration (Annotation Scanning)
+	private static void testApplicationContext01() {
+		BeanFactory ac = new ClassPathXmlApplicationContext("com/douzone/container/config/user");
+		
+		User1 user1 = ac.getBean(User1.class);
+		System.out.println(user1.getName());
+		
+		
+		// Bean id가 자동으로 설정됨 
+		user1 = (User1)ac.getBean("user1");
+		System.out.println(user1.getName());
+	}
+	
+	//XML Auto Configuration (Annotation Scanning)
+	private static void testBeanFactory01() {
+		BeanFactory bf = new XmlBeanFactory ( new ClassPathResource("com/douzone/container/config/user"));
+		
+		User1 user1 = bf.getBean(User1.class);
+		System.out.println(user1.getName());
+		
+		
+		// Bean id가 자동으로 설정됨 
+		user1 = (User1)bf.getBean("user1");
+		System.out.println(user1.getName());
+	}
+	
+	//XML Bean Configuration (Explicit Configuration)
+	private static void testBeanFactory02() {
+		BeanFactory bf = new XmlBeanFactory ( new ClassPathResource("com/douzone/container/config/user"));
+		
+		User1 user1 = bf.getBean(User1.class);
+		System.out.println(user1.getName());
+		
+		//Bean id가 자동으로 설정되지 않음 => 명시적 설정 필요  
+		user1 = (User1)bf.getBean("user1");
+		System.out.println(user1.getName());
+	}
+	
+	
+}
+
+
